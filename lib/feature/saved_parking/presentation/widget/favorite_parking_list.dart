2e51eb5f6e250df6565b56d557a23aae750e353b
Xheghun <ysenruf@gmail.com>
2021-04-23 10:54:44 +0400

    saved parking list

--- lib/feature/saved_parking/presentation/widget/favorite_parking_list.dart
+++ lib/feature/saved_parking/presentation/widget/favorite_parking_list.dart
@@ -1,25 +1,64 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:simple_parking/app/res/colors.dart';
 import 'package:simple_parking/app/res/string.dart';
 import 'package:simple_parking/app/res/style.dart';
 import 'package:simple_parking/feature/saved_parking/presentation/widget/widget.dart';
 
 class FavoriteParkingList extends StatelessWidget {
-
-  Widget _distance() {
-    return ();
-  }
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+
+    Widget _distance() {
+      return Container(
+        child: Row(
+          children: [
+            Icon(
+              Icons.directions_run,
+              size: 15,
+              color: colorPrimary,
+            ),
+            Text(
+              "4 km",
+              style: theme.textTheme.subtitle2,
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget _location() {
+      return Container(
+        child: Row(
+          children: [
+            Text(
+              "Downtown Dubai",
+              style: theme.textTheme.subtitle2,
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            Icon(
+              Icons.location_on,
+              size: 14,
+              color: colorPrimary,
+            )
+          ],
+        ),
+      );
+    }
+
     return ListView.separated(
       itemCount: 12,
+      padding: EdgeInsets.symmetric(vertical: 12),
       separatorBuilder: (_, __) => Container(
         height: 6,
       ),
       itemBuilder: (context, index) {
         return ListTile(
+          onTap: () {},
           contentPadding: EdgeInsets.zero,
           leading: ClipRRect(
             borderRadius: AppStyle.borderRadius6,
@@ -37,11 +76,13 @@ class FavoriteParkingList extends StatelessWidget {
             "Dubai Mall",
             style: theme.textTheme.headline2,
           ),
-          subtitle: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+          subtitle: Column(
             children: [
-              RatingBarWidget(),
-
+              _location(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [RatingBarWidget(), _distance()],
+              ),
             ],
           ),
         );
