c50664b2c32d79246ffc1a6d99bacd9af14e212c
Xheghun <ysenruf@gmail.com>
2021-04-23 20:17:48 +0400

    nearby parking places remote data

---
+++ lib/feature/parking_spot/data/data_sources/remote/parking_spot_remote_datasource.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:simple_parking/feature/parking_spot/data/models/parking_place_model.dart';
+import 'package:simple_parking/feature/parking_spot/domain/entities/location.dart';
+import 'package:simple_parking/feature/parking_spot/domain/entities/parking_place.dart';
+import 'package:simple_parking/feature/parking_spot/error/server_error.dart';
+import 'package:meta/meta.dart';
+import 'package:simple_parking/keys.dart';
+
+abstract class ParkingSpotRemoteDataSource {
+  //get a list of nearby parking from the google places api
+  ///throws a [ServerError] if an error occurs
+  Future<ParkingPlace> getNearbyParking(Location location);
+}
+
+class ParkingSpotRemoteDataSourceImpl implements ParkingSpotRemoteDataSource {
+  final Dio client;
+
+  ParkingSpotRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<ParkingPlace> getNearbyParking(Location location) async {
+    String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+
+    Map<String, dynamic> queryParam = {
+      "rankby": "distance",
+      "type": "parking",
+      "key": GOOGLE_API_KEY,
+      "location": "${location.lat},${location.lng}"
+    };
+
+    try {
+      final response = await client.get(url, queryParameters: queryParam);
+      if (response.statusCode == 200) {
+        return ParkingPlaceModel.fromJson(response.data);
+      }
+    } on DioError catch (error) {
+      print(error.message);
+    }
+    throw ServerError();
+  }
+}
---
+++ lib/feature/parking_spot/error/server_error.dart
@@ -0,0 +1,1 @@
+class ServerError implements Exception {}
