d90d96121697dee2dcf7c1f0142efa437d36b498
Xheghun <ysenruf@gmail.com>
2021-04-24 18:54:41 +0400

    add save parking to get parking use case

--- lib/feature/parking_spot/domain/use_case/get_parking_data.dart
+++ lib/feature/parking_spot/domain/use_case/get_parking_data.dart
@@ -3,13 +3,19 @@ import 'package:dartz/dartz.dart';
 import 'package:simple_parking/feature/parking_spot/domain/entities/location.dart';
 import 'package:simple_parking/core/entities/parking_place.dart';
 import 'package:simple_parking/feature/parking_spot/domain/repositories/parking_location_repository.dart';
-
+import 'package:simple_parking/feature/saved_parking/domain/repositories/saved_parking_repo.dart';
+import 'package:simple_parking/feature/saved_parking/domain/use_case/saved_parking_usecases_impl.dart';
+import 'package:meta/meta.dart';
 import 'parking_location_data.dart';
 
-class GetParkingLocationData implements ParkingLocationData {
+class GetParkingLocationData
+    implements ParkingLocationData, SavedParkingUseCasesImpl {
   final ParkingLocationRepository parkingLocationRepository;
+  final SavedParkingLocalRepository savedParkingLocalRepository;
 
-  GetParkingLocationData(this.parkingLocationRepository);
+  GetParkingLocationData(
+      {@required this.parkingLocationRepository,
+      @required this.savedParkingLocalRepository});
 
   @override
   Future<Location> getUserPosition() =>
@@ -19,4 +25,13 @@ class GetParkingLocationData implements ParkingLocationData {
   Future<Either<Failure, List<ParkingPlace>>> getNearbyParking(
           Location location) =>
       parkingLocationRepository.getNearbyParking(location);
+
+  @override
+  Future<Either<CacheFailure, bool>> savePlace(ParkingPlace place) =>
+      savedParkingLocalRepository.savePlace(place);
+
+  @override
+  Future<Either<CacheFailure, List<ParkingPlace>>> savedPlaces() {
+    throw UnimplementedError("tried to get saved places outside it's scope");
+  }
 }
--- lib/feature/saved_parking/domain/use_case/saved_parking_usecases_impl.dart
+++ lib/feature/saved_parking/domain/use_case/saved_parking_usecases_impl.dart
@@ -3,9 +3,12 @@ import 'package:simple_parking/core/entities/parking_place.dart';
 import 'package:simple_parking/core/failure/failure.dart';
 import 'package:simple_parking/feature/saved_parking/domain/repositories/saved_parking_repo.dart';
 import 'package:simple_parking/feature/saved_parking/domain/use_case/saved_parking_usecases.dart';
+import 'package:meta/meta.dart';
 
 class SavedParkingUseCasesImpl implements SavedParkingUseCases {
-  SavedParkingLocalRepository savedParkingLocalRepository;
+  final SavedParkingLocalRepository savedParkingLocalRepository;
+
+  SavedParkingUseCasesImpl({@required this.savedParkingLocalRepository});
   @override
   Future<Either<CacheFailure, bool>> savePlace(ParkingPlace place) =>
       savedParkingLocalRepository.savePlace(place);
