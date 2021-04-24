b1e271fe4fd32188e4ac51eccdb1ac02a506c261
Xheghun <ysenruf@gmail.com>
2021-04-24 15:06:53 +0400

    saved parking local datasource impl

--- lib/feature/saved_parking/data/data_sources/local/saved_parking_local_datascource.dart
+++ lib/feature/saved_parking/data/data_sources/local/saved_parking_local_datascource.dart
@@ -1,13 +1,18 @@
+import 'package:hive/hive.dart';
 import 'package:simple_parking/core/entities/parking_place.dart';
+import 'package:meta/meta.dart';
 
 abstract class SavedParkingLocalDataSource {
-  Future<bool> savePlace(ParkingPlace place);
+  Future<void> savePlace(ParkingPlace place);
   Future<List<ParkingPlace>> parkingPlace();
 }
 
 class SavedParkingPlaceLocalDataSourceImpl
     implements SavedParkingLocalDataSource {
-      
+  final Box box;
+
+  SavedParkingPlaceLocalDataSourceImpl({@required this.box});
+
   @override
   Future<List<ParkingPlace>> parkingPlace() {
     // TODO: implement parkingPlace
@@ -15,8 +20,7 @@ class SavedParkingPlaceLocalDataSourceImpl
   }
 
   @override
-  Future<bool> savePlace(ParkingPlace place) {
-    // TODO: implement savePlace
-    throw UnimplementedError();
+  Future<void> savePlace(ParkingPlace place) {
+   return box.put("parking", place);
   }
 }
