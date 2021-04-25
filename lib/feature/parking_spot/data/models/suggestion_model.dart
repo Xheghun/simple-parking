824ed896b3d72966849e2efc1f95e34c7a2d63b7
Xheghun <ysenruf@gmail.com>
2021-04-25 21:27:00 +0400

    added suggestion model

---
+++ lib/feature/parking_spot/data/models/suggestion_model.dart
@@ -0,0 +1,23 @@
+class SuggestionModel {
+    SuggestionModel({
+        this.description,
+        this.placeId,
+        this.reference,
+    });
+
+    String description;
+    String placeId;
+    String reference;
+
+    factory Suggestion.fromJson(Map<String, dynamic> json) => Suggestion(
+        description: json["description"] == null ? null : json["description"],
+        placeId: json["place_id"] == null ? null : json["place_id"],
+        reference: json["reference"] == null ? null : json["reference"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "description": description == null ? null : description,
+        "place_id": placeId == null ? null : placeId,
+        "reference": reference == null ? null : reference,
+    };
+}
