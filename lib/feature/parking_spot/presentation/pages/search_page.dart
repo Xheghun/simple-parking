7c47eda0e029479fbdac8136132e4ed8a568d8ff
Xheghun <ysenruf@gmail.com>
2021-04-25 22:47:10 +0400

    search

--- lib/feature/parking_spot/presentation/pages/search_page.dart
+++ lib/feature/parking_spot/presentation/pages/search_page.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_parking/app/locator.dart';
+import 'package:simple_parking/feature/parking_spot/domain/entities/suggestion.dart';
 import 'package:simple_parking/feature/parking_spot/presentation/viewmodel/search_suggestion_viewmodel.dart';
 import 'package:simple_parking/feature/parking_spot/presentation/widget/widget.dart';
 
 class SearchPage extends StatelessWidget {
   static const routeName = "/searchPage";
 
-  Widget body(SearchSuggestionViewmodel model) {
-    if (model.isBusy()) {
-     return Center(
-        child: CircularProgressIndicator(),
-      );
-    } else {
-      if(model.s)
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context);
     var theme = Theme.of(context);
+
+    Widget _body(SearchSuggestionViewmodel model) {
+      if (model.isBusy()) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      } else {
+        if (model.suggestions.isEmpty) {
+          return Center(
+            child: Text(
+              "results will appear here",
+              style: theme.textTheme.bodyText2,
+            ),
+          );
+        }
+        return ListView.builder(
+          itemCount: model.suggestions.length,
+          itemBuilder: (context, index) {
+            Suggestion suggestion = model.suggestions[index];
+            return ListTile(
+              contentPadding: EdgeInsets.zero,
+              leading: Icon(Icons.location_on),
+              title: Text(suggestion.description),
+            );
+          },
+        );
+      }
+    }
+
     return ChangeNotifierProvider(
       create: (_) => SearchSuggestionViewmodel(locator(), locator()),
       child: Consumer<SearchSuggestionViewmodel>(
@@ -39,12 +59,7 @@ class SearchPage extends StatelessWidget {
                   margin: EdgeInsets.zero,
                   hintText: "Enter city name",
                 )),
-            body: Center(
-              child: Text(
-                "results will appear here",
-                style: theme.textTheme.bodyText2,
-              ),
-            ),
+            body: _body(model),
           );
         },
       ),
