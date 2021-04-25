efb3f0cab99a7b01e857fb7b1c70017b9394918d
Xheghun <ysenruf@gmail.com>
2021-04-25 20:43:52 +0400

    search page

---
+++ lib/feature/parking_spot/presentation/pages/search_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  static const routeName = "/searchPage";
+
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          decoration: InputDecoration(
+            prefixIcon: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context),
+            ),
+            hintText: "Enter city name",
+          ),
+        ),
+      ),
+    );
+  }
+}
